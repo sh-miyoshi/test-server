c00a75a7c5465a0c5a1577eef3182f487440ef92
s.miyoshi <s.miyoshi1991@gmail.com>
2020-02-23 11:23:28 +0900

    add status handler

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"flag"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+var logger = log.New(os.Stderr, "[TESTSERVER]", log.LUTC|log.LstdFlags)
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("ok"))
+}
+
+func statusHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	statusStr := vars["statusCode"]
+
+	logger.Printf("Status handler called with staus code: %s", statusStr)
+
+	statusCode, err := strconv.Atoi(statusStr)
+	if err != nil {
+		http.Error(w, "Failed to convert to integer", http.StatusBadRequest)
+		return
+	}
+
+	http.Error(w, "Status Write", statusCode)
+}
+
+func main() {
+	var logFile string
+	var serverAddr string
+	flag.StringVar(&logFile, "logfile", "", "file name for log, output to stdout if empty")
+	flag.StringVar(&serverAddr, "server", "0.0.0.0:10000", "bind addr for server")
+	flag.Parse()
+
+	// output log to file
+	if logFile != "" {
+		file, err := os.Create(logFile)
+		if err != nil {
+			logger.Printf("failed to create logFile %v", logFile)
+			os.Exit(1)
+		}
+		//do not call file.Close() because logger write log through file.Writer
+
+		logger.SetOutput(file)
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", defaultHandler).Methods("GET")
+	r.HandleFunc("/status/{statusCode}", statusHandler)
+
+	logger.Println("Start Server")
+	if err := http.ListenAndServe(serverAddr, r); err != nil {
+		logger.Printf("%+v", err)
+		os.Exit(1)
+	}
+}
